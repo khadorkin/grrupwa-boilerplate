0464252a2c0a9741973f722a0288d935556188cf
Vu Hoang Minh <vuhminh.mac@gmail.com>
2016-06-29 22:51:10 +0700

    Update dev and prod client root file

--- src/client.js
+++ src/client.js
@@ -1,25 +1,27 @@
-import { AppContainer } from 'react-hot-loader';
+import { browserHistory, match, Router } from 'react-router/es6';
+import { loadCSS } from 'fg-loadcss';
+import { render } from 'react-dom';
 import IsomorphicRelay from 'isomorphic-relay';
 import IsomorphicRouter from 'isomorphic-relay-router';
 import React from 'react';
-import { render } from 'react-dom';
-import { browserHistory, match, Router } from 'react-router/es6';
 import Relay from 'react-relay';
+
 import routes from './routes';
-import { loadCSS } from 'fg-loadcss';
+import WithStylesContext from './helpers/WithStylesContext';
 import fetchWithRetries from '../node_modules/fbjs/lib/fetchWithRetries';
 
 // Asynchronously load non-critical CSS.
 // Components that have critical-css use withStyles decorator
 if (!__DEV__) loadCSS('css/styles.css');
-const environment = new Relay.Environment();
-const DefaultNetworkLayer = new Relay.DefaultNetworkLayer('/graphql');
 
 /*
  * This portion is important for Offline mode.
  * We convert the requests from POST to GET, to allow Service Workers to cache
  * responses for offline usage. Must be a function expression to keep "this" reference
 */
+const environment = new Relay.Environment();
+const DefaultNetworkLayer = new Relay.DefaultNetworkLayer('/graphql');
+
 DefaultNetworkLayer._sendQuery = function modifiedSendQuery(request) {
   return fetchWithRetries(`/graphql?query=${request.getQueryString()}&variables=${JSON.stringify(request.getVariables())}`, {
     ...this._init,
@@ -37,17 +39,28 @@ IsomorphicRelay.injectPreparedData(environment, data);
 
 const rootEl = document.getElementById('root');
 
-match({ routes, history: browserHistory }, (error, redirectLocation, renderProps) => {
-  IsomorphicRouter.prepareInitialRender(environment, renderProps).then(props => {
-    render((
-      <AppContainer>
-        <Router {...props} />
-      </AppContainer>
-    ), rootEl);
+if (!__DEV__) {
+  match({ routes, history: browserHistory }, (error, redirectLocation, renderProps) => {
+    IsomorphicRouter.prepareInitialRender(environment, renderProps).then(props => {
+      render((
+        <WithStylesContext onInsertCss={() => {}}>
+          <Router {...props} />
+        </WithStylesContext>
+      ), rootEl);
+    });
   });
-});
+} else if (__DEV__ && module.hot) {
+  const { AppContainer } = require('react-hot-loader');
+  match({ routes, history: browserHistory }, (error, redirectLocation, renderProps) => {
+    IsomorphicRouter.prepareInitialRender(environment, renderProps).then(props => {
+      render((
+        <AppContainer>
+          <Router {...props} />
+        </AppContainer>
+      ), rootEl);
+    });
+  });
 
-if (__DEV__ && module.hot) {
   module.hot.accept('./routes', () => {
     const nextRoutes = require('./routes').default;
 
