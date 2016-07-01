e819e783b89ebd5f3a2a17b2d84d411f0301ec44
Vu Hoang Minh <vuhminh.mac@gmail.com>
2016-07-01 16:43:52 +0700

    Import css only in production

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -42,7 +42,7 @@ const Html = ({ markup, preloadedData, head, criticalCss, assets }: Props) => (
       {/* This snippet is taken from loadCSS.
         We inline it here to async load css files below */}
       <script dangerouslySetInnerHTML={{ __html: '!function(e){"use strict";var n=function(n,t,o){function i(e){return a.body?e():void setTimeout(function(){i(e)})}function r(){l.addEventListener&&l.removeEventListener("load",r),l.media=o||"all"}var d,a=e.document,l=a.createElement("link");if(t)d=t;else{var s=(a.body||a.getElementsByTagName("head")[0]).childNodes;d=s[s.length-1]}var f=a.styleSheets;l.rel="stylesheet",l.href=n,l.media="only x",i(function(){d.parentNode.insertBefore(l,t?d:d.nextSibling)});var u=function(e){for(var n=l.href,t=f.length;t--;)if(f[t].href===n)return e();setTimeout(function(){u(e)})};return l.addEventListener&&l.addEventListener("load",r),l.onloadcssdefined=u,u(r),l};"undefined"!=typeof exports?exports.loadCSS=n:e.loadCSS=n}("undefined"!=typeof global?global:this);' }} />
-      <script dangerouslySetInnerHTML={{ __html: 'loadCSS(\'/css/styles.css\');' }} />
+      {!__DEV__ && <script dangerouslySetInnerHTML={{ __html: 'loadCSS(\'/css/styles.css\');' }} />}
     </head>
     <body>
       <div id="root" dangerouslySetInnerHTML={{ __html: markup }} />
